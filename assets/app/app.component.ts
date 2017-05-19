db29a10329239780951ff8a32492b7a2c7e77465
architMahto <architmahto@cmail.carleton.ca>
2017-05-19 08:07:23 -0400

    Added content markup to AppComponent.

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [
+      `
+        .author {
+          display: inline-block;
+          font-size: 12px;
+          font-style: italic;
+          width: 80%;
+        }
+        .config {
+          display: inline-block;
+          font-size: 12px;
+          text-align: right;
+          width: 19%;
+        }
+      `
+    ]
 })
 export class AppComponent {
-    
+  message = {
+    content: 'A message',
+    author: 'Max'
+  } 
 }
