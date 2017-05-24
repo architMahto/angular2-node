c6e2bdecf0610c712efa0f42cf220e2340a2dd9b
architMahto <architmahto@cmail.carleton.ca>
2017-05-24 18:07:53 -0400

    Added MessageComponent and MessageContainerComponent.

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -3,22 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styles: [
-      `
-        .author {
-          display: inline-block;
-          font-size: 12px;
-          font-style: italic;
-          width: 80%;
-        }
-        .config {
-          display: inline-block;
-          font-size: 12px;
-          text-align: right;
-          width: 19%;
-        }
-      `
-    ]
 })
 export class AppComponent {
   message = {
---
+++ assets/app/components/messages/message/message.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Message} from '../../../models';
+
+@Component({
+  selector: 'app-message',
+  styleUrls: ['./message.component.css'],
+  templateUrl: './message.component.html'
+})
+
+export class MessageComponent {
+  @Input()
+  message: Message;
+
+  @Output()
+  editClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  onEdit() {
+    this.editClicked.emit('A new value');
+  }
+}
