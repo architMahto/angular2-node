00eda5a8a2f48e87a8296e302cacf2b774ccf5d2
architMahto <architmahto@cmail.carleton.ca>
2017-06-05 18:46:19 -0400

    Added functionality for logging in and logging out in the front end.

--- assets/app/features/auth/auth.component.ts
+++ assets/app/features/auth/auth.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '../../services';
+
+
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
 })
 
-export class AuthComponent {}
+export class AuthComponent {
+    constructor(private authService: AuthService) {}
+
+    isLoggedIn() {
+        return this.authService.isLoggedIn();
+    }
+}
--- assets/app/features/auth/signin/components/signin/signin.component.ts
+++ assets/app/features/auth/signin/components/signin/signin.component.ts
@@ -1,5 +1,9 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from '../../../../../models';
+import { AuthService } from '../../../../../services';
 
 @Component({
   selector: 'app-signin',
@@ -10,8 +14,22 @@ import { Component } from '@angular/core';
 export class SigninComponent {
   signinForm: FormGroup;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
   onSubmit() {
-    console.log(this.signinForm);
+    const user: User = {
+      email: this.signinForm.controls['email'].value,
+      password: this.signinForm.controls['password'].value,
+    };
+    this.authService.signIn(user)
+      .subscribe(
+        data => { 
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('userId', data.userId);
+          this.router.navigateByUrl('/');
+        },
+        error => { console.log(error); }
+      ) 
     this.signinForm.reset();
   }
 
